6912c4aea18491edfccbc2fb788e3018d54014c3
Aayush <aayushagrawal97@gmail.com>
2024-05-14 17:30:56 +0530

    interface

---
+++ Ginnis/Ginnis.WebAPIs/Controllers/ImagesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ginnis.WebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+    }
+}
